8a48033d083994e9adbd0da5b966171306665432
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-17 11:13:13 +0900

    ㅇ

--- portfolio/my-portfolio/src/components/InfoSection.jsx
+++ portfolio/my-portfolio/src/components/InfoSection.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import "../css/components/infosection.scss"
 
 export default function InfoSection() {
   return (
-    <div className="hero  ">
-      <div className="hero-content flex-col lg:flex-row shadow-2xl mx-auto my-0">
-        <img src="/public/image/mimo.png" className="max-w-sm rounded-lg " />
-        <div>
-          <h1 className="text-4xl font-bold">황대웅</h1>
-          <h1 className="text-5xl font-bold">FRONT-EDN DEVELOPER!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
+    <div className="my-info">
+      <section >
+        <img src="./public/image/mimo.png" alt="it's me!" />
+      </section>
+      <section>
+        <strong>황대웅</strong>
+        <span>FRONT-END DEVELOPER</span>
+      </section>
     </div>
   );
 }
