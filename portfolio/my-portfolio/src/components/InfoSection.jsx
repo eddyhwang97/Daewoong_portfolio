9a67fd51e92afe0bd113bf0105316caa1c9179e5
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-31 13:59:19 +0900

    f

--- portfolio/my-portfolio/src/components/InfoSection.jsx
+++ portfolio/my-portfolio/src/components/InfoSection.jsx
@@ -3,14 +3,6 @@ import "../css/components/infosection.scss"
 
 export default function InfoSection() {
   return (
-    <div className="my-info">
-      <section >
-        <img src="./public/image/mimo.png" alt="it's me!" />
-      </section>
-      <section>
-        <strong>황대웅</strong>
-        <span>FRONT-END DEVELOPER</span>
-      </section>
-    </div>
+    <></>
   );
 }
