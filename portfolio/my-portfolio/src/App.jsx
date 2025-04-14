952236c7de442691f7e89cd692cd6f38c17e7111
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-14 18:46:14 +0900

    d

--- portfolio/my-portfolio/src/App.jsx
+++ portfolio/my-portfolio/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='columns-7xl'></div>
+      <div className="container">
+        <section className="tabs-container">
+          <div className="tab"></div>
+          <div className="tab"></div>
+        </section>
+        <section className="about-me"></section>
+        <section className="project"></section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
