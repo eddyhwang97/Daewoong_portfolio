b992aea2011fb01eaf4ba845733bd74c6fdb328f
eddyhwang97 <hwangdw1997@gmail.com>
2025-05-15 23:53:27 +0900

    .

--- portfolio/my-portfolio/src/App.jsx
+++ portfolio/my-portfolio/src/App.jsx
@@ -5,12 +5,16 @@ function App() {
   return (
     <>
       <div className="container">
-        <section className="tabs-container">
-          <div className="tab"></div>
-          <div className="tab"></div>
-        </section>
-        <section className="about-me"></section>
-        <section className="project"></section>
+        
+       <section className="section section-1">
+        <div>사진 및 어떤 개발자가 되고 싶은지</div>
+       </section>
+       <section className="section section-2">
+        내가 가진 기술 스택
+       </section>
+       <section className="section section-3">
+        내가 만든 프로젝트
+       </section>
       </div>
     </>
   );
