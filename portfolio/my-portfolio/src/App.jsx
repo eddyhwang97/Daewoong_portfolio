60fbbc53467edb2eef6d95803e033b484dad5d75
eddyhwang97 <hwangdw1997@gmail.com>
2025-05-16 23:20:23 +0900

    .

--- portfolio/my-portfolio/src/App.jsx
+++ portfolio/my-portfolio/src/App.jsx
@@ -1,20 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import ProjectDetail from "./components/ProjectDetail";
 
 function App() {
+  const [projects, setProjects] = useState(false);
+  useEffect(() => {
+    if (projects) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [projects]);
   return (
     <>
       <div className="container">
-        
-       <section className="section section-1">
-        <div>사진 및 어떤 개발자가 되고 싶은지</div>
-       </section>
-       <section className="section section-2">
-        내가 가진 기술 스택
-       </section>
-       <section className="section section-3">
-        내가 만든 프로젝트
-       </section>
+        <section className="section section-1">
+          <div className="intro-title">
+            <p>배우는게 즐겁고, 끊임없이 개선하는</p>
+            <p>개발자 황대웅 입니다.</p>
+          </div>
+          <div className="profile">
+            <div className="profile-image">
+              <img src="" alt="" />
+            </div>
+            <div className="profile-info">
+              <h1 className="profile-name">황대웅</h1>
+              <p className="profile-email">Email : hwangdw97@gmail.com</p>
+              <div className="profile-contacts">
+                <a href="https://github.com/eddyhwang97" target="_blank" rel="noreferrer noopener">
+                  GitHub
+                </a>
+                <a href="https://velog.io/@eddyhwang97/posts" target="_blank" rel="noreferrer noopener">
+                  Velog
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section section-2">
+          <div>내가 가진 기술 스택</div>
+        </section>
+        <section className="section section-3">
+          <div>PROJECT</div>
+          <div className="projects">
+            <article className="project" onClick={() => setProjects(true)}>
+              <div className="project-thumbnail">
+                <img src="" alt="썸네일" />
+              </div>
+              <h2 className="project-title">프로젝트 제목</h2>
+            </article>
+            <article className="project" onClick={() => setProjects(true)}>
+              <div className="project-thumbnail">
+                <img src="" alt="썸네일" />
+              </div>
+              <h2 className="project-title">프로젝트 제목</h2>
+            </article>
+            <article className="project" onClick={() => setProjects(true)}>
+              <div className="project-thumbnail">
+                <img src="" alt="썸네일" />
+              </div>
+              <h2 className="project-title">프로젝트 제목</h2>
+            </article>
+            <article className="project" onClick={() => setProjects(true)}>
+              <div className="project-thumbnail">
+                <img src="" alt="썸네일" />
+              </div>
+              <h2 className="project-title">프로젝트 제목</h2>
+            </article>
+          </div>
+          {projects === true && <ProjectDetail />}
+        </section>
       </div>
     </>
   );
---
+++ portfolio/my-portfolio/src/components/ProjectDetail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ProjectDetail.css";
+
+export default function ProjectDetail() {
+  return (
+    <div className="project-container">
+      <div>ProjectDetail</div>
+    </div>
+  );
+}
